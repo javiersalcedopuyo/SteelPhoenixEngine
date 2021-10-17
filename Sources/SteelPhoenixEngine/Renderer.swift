44b8a2e31605b11c1c3f771a811a1228df626e40
Javier Salcedo <javiersalcedo.p@gmail.com>
2021-10-17 22:35:12 +0100

    Load obj models

--- Sources/SteelPhoenixEngine/Renderer.swift
+++ Sources/SteelPhoenixEngine/Renderer.swift
@@ -8,6 +8,9 @@ let UNIFORM_BUFFER_INDEX = 1
 
 let WORLD_UP = Vector3(x:0, y:1, z:0)
 
+let TEST_MODEL_NAME = "bunny"
+let TEST_MODEL_EXTENSION = "obj"
+
 public class Renderer : NSObject
 {
     public  var mView:          MTKView
@@ -15,8 +18,7 @@ public class Renderer : NSObject
     private let mCommandQueue:  MTLCommandQueue
     private let mPipelineState: MTLRenderPipelineState
 
-    private let mIndexBuffer:   MTLBuffer?
-
+    private var mModel:         Model?
     // TODO: Load textures on demand
     private var mTexture:       MTLTexture?
     // TODO: Pre-built collection?
@@ -24,51 +26,13 @@ public class Renderer : NSObject
 
     private var mDepthStencilState: MTLDepthStencilState?
 
-    // TODO: use an array of Vertex
-    // TODO: Load from file
-    // TODO: Load models on demand
-    let indices: [UInt16] = [ 0, 1, 2, 2, 3, 0 ]
-    let vertexData: [SIMD3<Float>] =
-    [
-        // v0
-        [-0.5, -0.5, 0.0 ], // position
-        [ 1.0,  0.0, 0.0 ], // color
-        [ 0.0,  1.0, 0.0 ], // UVs TODO: use just 2 floats
-        // v1
-        [ 0.5, -0.5, 0.0 ],
-        [ 0.0,  1.0, 0.0 ],
-        [ 1.0,  1.0, 0.0 ],
-        // v2
-        [ 0.5,  0.5, 0.0 ],
-        [ 0.0,  0.0, 1.0 ],
-        [ 1.0,  0.0, 0.0 ],
-        // v3
-        [-0.5,  0.5, 0.0 ],
-        [ 1.0,  1.0, 1.0 ],
-        [ 0.0,  0.0, 0.0 ]
-    ]
-    let vertexData2: [SIMD3<Float>] =
-    [
-        // v0
-        [-0.5, -0.5, 0.5 ], // position
-        [ 1.0,  0.0, 0.0 ], // color
-        [ 0.0,  1.0, 0.0 ], // UVs TODO: use just 2 floats
-        // v1
-        [ 0.5, -0.5, 0.5 ],
-        [ 0.0,  1.0, 0.0 ],
-        [ 1.0,  1.0, 0.0 ],
-        // v2
-        [ 0.5,  0.5, 0.5 ],
-        [ 0.0,  0.0, 1.0 ],
-        [ 1.0,  0.0, 0.0 ],
-        // v3
-        [-0.5,  0.5, 0.5 ],
-        [ 1.0,  1.0, 1.0 ],
-        [ 0.0,  0.0, 0.0 ]
-    ]
-
     public init(mtkView: MTKView)
     {
+        if mtkView.device == nil
+        {
+            fatalError("NO GPU!")
+        }
+
         mView = mtkView
         mView.depthStencilPixelFormat = MTLPixelFormat.depth16Unorm
         mView.clearDepth              = 1.0
@@ -93,31 +57,21 @@ public class Renderer : NSObject
         let vertexFunction   = library.makeFunction(name: "vertex_main")
         let fragmentFunction = library.makeFunction(name: "fragment_main")
 
-        let vertDesc = MTLVertexDescriptor()
-        // Position
-        vertDesc.attributes[0].format      = .float3
-        vertDesc.attributes[0].bufferIndex = VERTEX_BUFFER_INDEX
-        vertDesc.attributes[0].offset      = 0
-        // Color
-        vertDesc.attributes[1].format      = .float3
-        vertDesc.attributes[1].bufferIndex = VERTEX_BUFFER_INDEX
-        vertDesc.attributes[1].offset      = MemoryLayout<SIMD3<Float>>.stride
-        // TODO: Normals
-        vertDesc.attributes[2].format      = .float3
-        vertDesc.attributes[2].bufferIndex = VERTEX_BUFFER_INDEX
-        vertDesc.attributes[2].offset      = 0 // TODO: MemoryLayout<SIMD3<Float>>.stride
-        // UVs
-        vertDesc.attributes[3].format      = .float2
-        vertDesc.attributes[3].bufferIndex = VERTEX_BUFFER_INDEX
-        vertDesc.attributes[3].offset      = MemoryLayout<SIMD3<Float>>.stride * 2
-
-        vertDesc.layouts[0].stride         = MemoryLayout<SIMD3<Float>>.stride * 3
+        if let modelURL = Bundle.module.url(forResource: TEST_MODEL_NAME,
+                                            withExtension: TEST_MODEL_EXTENSION)
+        {
+            mModel = Model(device: mtkView.device!, url: modelURL)
+        }
+        else
+        {
+            SimpleLogs.ERROR("Couldn't load model '" + TEST_MODEL_NAME + "." + TEST_MODEL_EXTENSION + "'")
+        }
 
         let pipelineDescriptor = MTLRenderPipelineDescriptor()
         pipelineDescriptor.colorAttachments[0].pixelFormat = mtkView.colorPixelFormat
         pipelineDescriptor.vertexFunction                  = vertexFunction
         pipelineDescriptor.fragmentFunction                = fragmentFunction
-        pipelineDescriptor.vertexDescriptor                = vertDesc
+        pipelineDescriptor.vertexDescriptor                = mModel?.mVertexDescriptor
         pipelineDescriptor.depthAttachmentPixelFormat      = mView.depthStencilPixelFormat
 
         guard let ps = try! mView.device?.makeRenderPipelineState(descriptor: pipelineDescriptor) else
@@ -131,14 +85,6 @@ public class Renderer : NSObject
         depthStencilDesc.isDepthWriteEnabled  = true
 
         mDepthStencilState = mView.device?.makeDepthStencilState(descriptor: depthStencilDesc)
-
-        mIndexBuffer = mView.device?.makeBuffer(bytes: indices,
-                                                length: indices.count * MemoryLayout.size(ofValue: indices[0]),
-                                                options: [])
-        if mIndexBuffer == nil
-        {
-            SimpleLogs.ERROR("Failed creating index buffer!")
-        }
 
         super.init()
 
@@ -158,18 +104,12 @@ public class Renderer : NSObject
 
     func render()
     {
-        let dataSize     = vertexData.count * MemoryLayout<SIMD3<Float>>.size
-        let vertexBuffer = mView.device?.makeBuffer(bytes: vertexData,
-                                                    length: dataSize,
-                                                    options: [])
-        let vertexBuffer2 = mView.device?.makeBuffer(bytes: vertexData2,
-                                                    length: dataSize,
-                                                    options: [])
+        let vertexBuffer = mModel?.mMeshes[0].vertexBuffers[0].buffer
 
         // TODO: Use Constant Buffer?
         var ubo   = UniformBufferObject()
         ubo.model = Matrix4x4.identity()
-        ubo.view  = Matrix4x4.lookAtLH(eye: Vector3(x:1, y:1, z:-1),
+        ubo.view  = Matrix4x4.lookAtLH(eye: Vector3(x:0, y:0.25, z:0.5),
                                        target: Vector3.zero(),
                                        upAxis: WORLD_UP)
         ubo.proj  = Matrix4x4.perspectiveLH(fovy: SLA.deg2rad(45.0),
@@ -187,48 +127,25 @@ public class Renderer : NSObject
         let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: mView.currentRenderPassDescriptor!)
         commandEncoder?.setRenderPipelineState(mPipelineState)
         commandEncoder?.setDepthStencilState(mDepthStencilState)
-        commandEncoder?.setFrontFacing(.counterClockwise)
+        commandEncoder?.setFrontFacing(.clockwise) // TODO: According to model?
         commandEncoder?.setCullMode(.back)
         commandEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: VERTEX_BUFFER_INDEX)
         commandEncoder?.setVertexBuffer(uniformBuffer, offset: 0, index: UNIFORM_BUFFER_INDEX)
         commandEncoder?.setFragmentTexture(mTexture, index: 0)
         commandEncoder?.setFragmentSamplerState(mSamplerState, index: 0)
 
-        if mIndexBuffer != nil
+        if (mModel != nil)
         {
-            commandEncoder?.drawIndexedPrimitives(type: .triangle,
-                                                  indexCount: 6,
-                                                  indexType: .uint16,
-                                                  indexBuffer: mIndexBuffer!,
-                                                  indexBufferOffset: 0)
-        }
-        else
-        {
-            SimpleLogs.WARNING("No index buffer!")
-            commandEncoder?.drawPrimitives(type: .triangle,
-                                           vertexStart: 0,
-                                           vertexCount: vertexData.count / 2,
-                                           instanceCount: 1)
+            for submesh in mModel!.mMeshes[0].submeshes
+            {
+                commandEncoder?.drawIndexedPrimitives(type: submesh.primitiveType,
+                                                      indexCount: submesh.indexCount,
+                                                      indexType: submesh.indexType,
+                                                      indexBuffer: submesh.indexBuffer.buffer,
+                                                      indexBufferOffset: submesh.indexBuffer.offset)
+            }
         }
 
-        commandEncoder?.setVertexBuffer(vertexBuffer2, offset: 0, index: VERTEX_BUFFER_INDEX)
-
-        if mIndexBuffer != nil
-        {
-            commandEncoder?.drawIndexedPrimitives(type: .triangle,
-                                                  indexCount: 6,
-                                                  indexType: .uint16,
-                                                  indexBuffer: mIndexBuffer!,
-                                                  indexBufferOffset: 0)
-        }
-        else
-        {
-            SimpleLogs.WARNING("No index buffer!")
-            commandEncoder?.drawPrimitives(type: .triangle,
-                                           vertexStart: 0,
-                                           vertexCount: vertexData.count / 2,
-                                           instanceCount: 1)
-        }
         commandEncoder?.endEncoding()
 
         commandBuffer.present(mView.currentDrawable!)
